d620ffc7025ed2d758a6f77e0ec678c3c99c9414
Zulfat Miftahutdinov <zulfatmi@gmail.com>
2020-07-12 11:58:59 +0300

    initial

---
+++ umls_ontology.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+
+class UMLSOntology:
+
+    def __init__(self, mrconso_path, mrsty_path, mrrel_path):
+        self.mrconso = self.read_mrconso(mrconso_path)
+        self.mrsty = self.read_mrsty(mrsty_path)
+        self.mrrel = self.read_mrrel(mrrel_path)
+
+    def read_mrconso(self, fpath):
+        """
+        :param fpath: path to to MRCONSO.RRF file
+        :return: DataFrame containing definitions of the concepts and synonyms
+        """
+        columns = ['CUI', 'LAT', 'TS', 'LUI', 'STT', 'SUI', 'ISPREF', 'AUI', 'SAUI', 'SCUI', 'SDUI', 'SAB', 'TTY', 'CODE',
+               'STR', 'SRL', 'SUPPRESS', 'CVF', 'NOCOL']
+        return pd.read_csv(fpath, names=columns, sep='|', encoding='utf-8')
+
+    def read_mrsty(self, fpath):
+        """
+        :param fpath: path to the MRSTY.RRF file
+        :return: DataFrame containing semantic types of the concepts
+        """
+        columns = ['CUI', 'TUI', 'STN', 'STY', 'ATUI', 'CVF', 'NOCOL']
+        return pd.read_csv(fpath, names=columns, sep='|', encoding='utf-8')
+
+    def read_mrrel(self, fpath):
+        """
+        :param fpath: path to to  the MRREL.RRF file
+        :return: DataFrame containing relations between concepts
+        """
+        columns = ['CUI1', 'AUI1', 'STYPE1', 'REL', 'CUI2', 'AUI2', 'STYPE2', 'RELA', 'RUI', 'SRUI', 'SAB', 'SL', 'RG',
+                   'DIR', 'SUPPRESS', 'CVF', 'NOCOL']
+        return pd.read_csv(fpath, names=columns, sep='|', encoding='utf-8')
+
+    def synonyms(self, concept_id, ontology):
+        """
+        :param concept_id: concept_id
+        :param ontology: ontology name
+        :return: synonyms by concept_id in a specific ontology
+        """
+        if ontology == 'UMLS':
+            return self.mrconso[self.mrconso.CUI == concept_id]
+        return self.mrconso[(self.mrconso.CODE == concept_id) & (self.mrconso.SAB == ontology)]
+
+    def get_parent_node(self, concept_id):
+        """
+        :param concept_id: CUI
+        :return: all parent CUIS for the concept_id
+        """
+        self.mrrel[(self.mrrel.CUI1 == concept_id) & (self.mrrel.REL == 'PAR')].CUI2.tolist()
+
+    def get_child_nodes(self, concept_id):
+        """
+        :param concept_id: CUI
+        :return: all child CUIS for the concept_id
+        """
+        self.mrrel[(self.mrrel.CUI1 == concept_id) & (self.mrrel.REL == 'CHD')].CUI2.tolist()
